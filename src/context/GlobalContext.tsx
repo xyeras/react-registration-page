ed9fbe074d33e97e5915f93ecc46e71769557773
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-05-20 18:35:12 -0700

    feat(registration-api): made app able to make post request to api but still it's broken

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -1,13 +1,20 @@
 import React, { createContext, useState, useReducer } from 'react';
+import { useParams } from 'react-router';
 import instance from '../api/apiConfig';
 
 // Initialize a default state for our app
 const initialState = {
-
+  users: [],
+  user: undefined,
+  getUsers: () => {},
+  addUser: () => {}
 };
 
 const appReducer = (state: any, action: any) => {
-
+  switch (action.type) {
+    case 'ADD_USER':
+      return { ...state, users: action.payload };
+  }
 };
 
 // Create Context from react
@@ -16,11 +23,34 @@ export const GlobalContext = createContext<InitialStateType>(initialState);
 // Create Global provider which will feed state to our components
 export const GlobalProvider: React.FC = ({ children }) => {
 
-  // const [state, dispatch] = useReducer(appReducer, initialState);
+  const [state, dispatch] = useReducer(appReducer, initialState);
+
+  const getUsers = async () => {
+    try {
+      let { data } = await instance.get('/');
+      dispatch({ type: 'ADD_USER', payload: data });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  const addUser = async (user:User) => {
+    try {
+      let { data } = await instance.post('/user', user);
+      dispatch({ type: 'ADD_USER', payload: data });
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
     <GlobalContext.Provider
       value={{
+        users: state.users,
+        user: state.user,
+        getUsers,
+        addUser
       }}>
       {children} {/* <AppRouter/> */}
     </GlobalContext.Provider>
--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,13 +1,116 @@
+import { useState, useContext, useEffect, FormEvent } from 'react';
 import Form from '../components/Form';
 import SuccessAlert from '../components/SuccessAlert'
+import { GlobalContext } from '../context/GlobalContext';
 
 const RegistrationPage = () => {
+
+    const { getUsers, addUser } = useContext(GlobalContext);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+      // getUsers();
+    }, [])
+
+    const handleSubmit = (event: FormEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+
+      addUser({firstName, lastName, email, password});
+
+      setSuccess(true);
+      setTimeout(() => {
+      setSuccess(false);
+      }, 3000);
+
+    }
 
     return (
       <div id="registration-page">
           <h2 className="display-6 text-center my-5">Registration</h2>
-          <Form />
-          <SuccessAlert/>
+
+          <form>
+          <div className="form-group row d-flex justify-content-center mt-3">
+            <label htmlFor="inputFirstName" className="col-sm-2 col-form-label">First name</label>
+            <div className="col-sm-4">
+              <input
+                type="text"
+                className="form-control"
+                id="inputFirstName"
+                value={firstName}
+                onChange={event => setFirstName(event.target.value)}
+                placeholder="First name"
+                required
+                />
+            </div>
+          </div>
+          <div className="form-group row d-flex justify-content-center mt-3">
+            <label htmlFor="inputLastName" className="col-sm-2 col-form-label">Last name</label>
+            <div className="col-sm-4">
+              <input
+                type="text"
+                className="form-control"
+                id="inputLastName"
+                value={lastName}
+                onChange={event => setLastName(event.target.value)}
+                placeholder="Last name"
+                required
+                />
+            </div>
+          </div>
+          <div className="form-group row d-flex justify-content-center mt-3">
+            <label htmlFor="inputEmail" className="col-sm-2 col-form-label">Email</label>
+            <div className="col-sm-4">
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail"
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+                placeholder="Email"
+                required/>
+            </div>
+          </div>
+          <div className="form-group row d-flex justify-content-center mt-3">
+            <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+            <div className="col-sm-4">
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword"
+                value={password}
+                onChange={event => setPassword(event.target.value)}
+                placeholder="Password"
+                required
+                />
+            </div>
+          </div>
+          <div className="row d-flex justify-content-center mt-5">
+            <button
+              type="button"
+              className="col-sm-3 btn btn-primary btn-md btn-block"
+              onClick={handleSubmit}
+              >
+                Submit
+            </button>
+          </div>
+        </form>
+
+        <div className="row d-flex justify-content-center mt-3">
+          {success ? (
+              <div>
+                  <div className='col-sm-4 alert alert-success text-center' role='alert'>
+                  You've successfully added a new user!
+                  </div>
+              </div>
+          ) : (
+              ''
+          )}
+        </div>
+
       </div>
     );
 };
